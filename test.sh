8e77a6628d0812285fad63c77a1048795e9cb6d9
Pavel Myshkov <pmyshkov@microsoft.com>
2025-07-18 13:38:41 +0100

    Add config for uv, ruff, tests (#10)

---
+++ test.sh
@@ -0,0 +1,17 @@
+# Exit immediately if anything goes wrong
+set -e
+
+# Style check
+uv run ruff format --check 
+
+# Organise imports
+uv run ruff check --select I 
+
+# Lint
+uv run ruff check
+
+# Type check (temporarily disabled)
+# uv run pyright
+
+# Tests
+uv run pytest
